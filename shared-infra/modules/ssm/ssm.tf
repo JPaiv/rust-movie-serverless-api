78d8e6e80d7c8bcc2eca9e36081f124dad8696bb
JPaiv <juho.paivarinta@outlook.com>
2022-02-19 22:58:07 +0200

    fIX NAME PATH.

--- shared-infra/modules/ssm/ssm.tf
+++ shared-infra/modules/ssm/ssm.tf
@@ -1,8 +1,6 @@
 resource "aws_ssm_parameter" "bucket" {
-  name        = "${var.environment}/${var.ssm_path}"
+  name        = "/${var.environment}/${var.ssm_path}"
   description = "Source file download bucket name for serverless lambda functions."
   type        = "String"
   value       = var.bucket_id
 }
-
-
