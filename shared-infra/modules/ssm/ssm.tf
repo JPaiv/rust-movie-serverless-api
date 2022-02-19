9bbd4e15dca0f6e3334894abc85861d1d95861bc
JPaiv <juho.paivarinta@outlook.com>
2022-02-19 22:36:13 +0200

    Add working directory.

--- shared-infra/modules/ssm/ssm.tf
+++ shared-infra/modules/ssm/ssm.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "bucket" {
   name        = "${var.environment}/${var.ssm_path}"
   description = "Source file download bucket name for serverless lambda functions."
-  type        = "SecureString"
+  type        = "String"
   value       = var.bucket_id
 }
 
