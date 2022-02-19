336aaa3512e5188071ca82d4a00a446adf667dcc
JPaiv <juho.paivarinta@outlook.com>
2022-02-20 01:15:14 +0200

    Update

--- shared-infra/modules/s3/s3.tf
+++ shared-infra/modules/s3/s3.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket_acl" "movie_data_source_file_bucket_versioning_acl" {
   acl    = "private"
 }
 
-resource "aws_s3_bucket_public_access_block" "app" {
+resource "aws_s3_bucket_public_access_block" "movie_data_source_file_bucket_public_access_block" {
   bucket = aws_s3_bucket.movie_data_source_file_bucket.id
 
   block_public_acls       = true
@@ -26,6 +26,15 @@ resource "aws_s3_bucket_public_access_block" "app" {
 
 resource "aws_s3_bucket" "movie_data_source_file_Log_bucket" {
   bucket = "${var.environment}-movie-source-data-bucket-tf-log-bucket"
+}
+
+resource "aws_s3_bucket_public_access_block" "movie_data_source_file_Log_buckett_public_access_block" {
+  bucket = aws_s3_bucket.movie_data_source_file_Log_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_acl" "log_bucket_acl" {
